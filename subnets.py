cb95e4220c9b8b9c733ef584efd5ca4a82ec0b08
tsarbomba0 <creepermaster000@gmail.com>
2024-09-18 07:35:09 +0200

    Remade the last parts of the code

--- subnets.py
+++ subnets.py
@@ -4,7 +4,7 @@ import re
 # variables for ip and number of hosts
 # TODO: Make a input system for this
 ip = [192, 168, 0, 2]
-hosts = [511, 24, 12, 2]
+hosts = [511, 255, 122, 24, 12, 2]
 
 # uses a logarithm to calculate the nearest power of 2 to generate a mask (except for 2)
 def calculateMask(n):
@@ -75,72 +75,43 @@ def networkAddress(address, mask, i=0):
     output = re.findall("........", binary_ip)
     return convertDecimal(output)
 
+# function to -1 the address
+def minusOneAddress(address):
+    if address[3] == 0:
+        address[3] = 255
+        address[2] -= 1
+    else:
+        address[3] = address[3] - 1
+    return address
+
+
 # variables 
 mask1 = calculateMask(hosts[0])
 wildcard = generateWildcard(mask1)
-network_address = networkAddress(ip, mask1)
+network_address1 = networkAddress(ip, mask1)
 
-
+result_to_return = []
 # function to calculate the subnets
-def calculateAll(hosts_num, addr, counter=0):
-    # calculate mask
-    mask = calculateMask(hosts_num)
-
-    # TODO: Get this shit organised!
-    if addr[3] >= 255:
-        leftover = addr[3] - 255
-        addr[3] = leftover
-        addr[2] += 1
-        if addr[2] >= 255:
-            leftover = addr[2] - 255
-            addr[2] = leftover
-            addr[1] += 1
-            if addr[1] >= 255:
-                leftover = addr[1] - 255
-                addr[1] = leftover
-                addr[0] += 1
-        net_addr = networkAddress(addr, mask)
-    else:
-        i=0
-        for part in addr:
-            addr[i] = int(addr[i])
-            i += 1
-        net_addr = addr
-    wildcard = generateWildcard(mask)
-    ######################################
-
-
-    # convert all str to int
-    i=0
-    for part in addr:
-        addr[i] = int(addr[i])
-        i += 1
+def calculateAll(hosts_num, addr, counter=0, output=""):
+    ms = calculateMask(hosts_num)
+    wildcard = generateWildcard(ms)
+    network_address = combine(addr, wildcard)
     
-    # remove overflows from octets
-    new_addr = combine(net_addr, wildcard)
-    if new_addr[3] >= 255:
-        leftover = addr[3] - 255
-        new_addr[3] = leftover
-        new_addr[2] += 1
-        if new_addr[2] >= 255:
-            leftover = addr[2] - 255
-            new_addr[2] = leftover
-            new_addr[1] += 1
-            if new_addr[1] >= 255:
-                leftover = addr[1] - 255
-                new_addr[1] = leftover
-                new_addr[0] += 1
-
-    # debug info
-    print(f'Counter: {counter} Mask: {mask} Wildcard: {wildcard} Netaddr: {net_addr} hostnum: {hosts[counter]} newaddr: {new_addr}')
-
-    # counter, if counter exceeds the amount of host options, ends function
-    counter = counter + 1
-    if counter > len(hosts)-1:
-        return 0
     
-    # recursive function
-    calculateAll(hosts[counter], new_addr, counter)
+    if network_address[3] > 254:
+        network_address[3] = 0
+        network_address[2] += 1
+    for part in network_address:
+        output = output + str(part) + "."
+    
+    result_to_return.append(output[:-1])
+
+    counter += 1
+    if counter == len(hosts):
+        print(result_to_return)
+        return result_to_return
+    calculateAll(hosts[counter], network_address, counter)
 
 # running the function
-calculateAll(hosts[0], network_address)
+a = calculateAll(hosts[0], network_address1)
+print(a)
