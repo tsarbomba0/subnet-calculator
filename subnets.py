b1d8083d38864dd68caf1b0838c088af1215f054
tsarbomba0 <creepermaster000@gmail.com>
2024-09-18 19:03:52 +0200

    Working calculator, gotta add a system to get user input in!

--- subnets.py
+++ subnets.py
@@ -2,9 +2,19 @@ import math
 import re
 
 # variables for ip and number of hosts
-# TODO: Make a input system for this
-ip = [192, 168, 0, 2]
+# TODO: Make a input system for this - input should be converted to network address!
+ip = [192, 168, 0, 0]
 hosts = [511, 255, 122, 24, 12, 2]
+
+# Saving first entry (network address)
+saved_ip=""
+for entry in ip:
+    entry = str(entry)
+    saved_ip = saved_ip + entry + "."
+saved_ip = saved_ip[:-1]
+
+# declaring list for later
+result_to_return = []
 
 # uses a logarithm to calculate the nearest power of 2 to generate a mask (except for 2)
 def calculateMask(n):
@@ -54,7 +64,8 @@ def combine(address, wildcard):
     for part in address:
         #print(wildcard)
         #print(address)
-        address[i] = address[i] + wildcard[i] 
+        address[i] = address[i] + wildcard[i]
+        
         i+=1
     return address
 
@@ -77,6 +88,8 @@ def networkAddress(address, mask, i=0):
 
 # function to -1 the address
 def minusOneAddress(address):
+    for part in address:
+        address[address.index(part)] = int(address[address.index(part)])
     if address[3] == 0:
         address[3] = 255
         address[2] -= 1
@@ -84,34 +97,94 @@ def minusOneAddress(address):
         address[3] = address[3] - 1
     return address
 
+# breaks up ip strings (ex. 172.16.0.1) into lists of integers (ex. [172, 16, 0 ,1])
+def breakupIntoInt(string):
+    array = []
+    string = string.split(".")
+    for part in string:
+        array.append(int(part))
+    return array
 
 # variables 
 mask1 = calculateMask(hosts[0])
 wildcard = generateWildcard(mask1)
 network_address1 = networkAddress(ip, mask1)
-
 result_to_return = []
+output_ips=[]
+output_masks=[]
+rebuilt_ip=""
+
+
+
+        
 # function to calculate the subnets
 def calculateAll(hosts_num, addr, counter=0, output=""):
+
+    # variables for mask, wildcard mask and network address
     ms = calculateMask(hosts_num)
     wildcard = generateWildcard(ms)
     network_address = combine(addr, wildcard)
+
+    # makes up for some loss in calculations
+    network_address[3] += 1 
     
-    
+    # removing overflows
     if network_address[3] > 254:
         network_address[3] = 0
         network_address[2] += 1
+
+    # converting number array into string
     for part in network_address:
         output = output + str(part) + "."
     
+    # appending results with cutting the last character (a extra dot)
     result_to_return.append(output[:-1])
+    result_to_return.append(ms)
 
+    # counter
     counter += 1
     if counter == len(hosts):
-        print(result_to_return)
-        return result_to_return
+        return 0
+    
+    # recursiveness
     calculateAll(hosts[counter], network_address, counter)
 
 # running the function
-a = calculateAll(hosts[0], network_address1)
-print(a)
+calculateAll(hosts[0], network_address1)
+
+
+# appending masks and ips to separate lists
+i=0
+for part in result_to_return:
+    if (i % 2) == 0:
+        output_ips.append(part)
+    else:
+        output_masks.append(part)
+    i+=1
+
+# inserting the saved ip from earlier (first input as network address) to the list of ips from the recursive function
+output_ips.insert(0, saved_ip)
+
+# main function that handles the printing part
+def __main__(i=0, count=0):
+    # for loop to handle the ips
+    for string in output_ips:
+        temp = minusOneAddress(breakupIntoInt(result_to_return[i % len(result_to_return)]))
+        output = ""
+        for element in temp:
+            output = output + str(element) + "."
+        output = output[:-1]
+        if i % 2 == 0:
+          i+=1
+        i+=1
+
+        # printing
+        print(f' Network address: {output_ips[count]} Broadcast: {output} Hosts: {hosts[count]} Unused hosts: {2**(32-output_masks[count])-hosts[count]-2}' )
+
+        # counter
+        count += 1
+        if count == len(output_ips)-1:
+            return 0
+
+
+__main__()
